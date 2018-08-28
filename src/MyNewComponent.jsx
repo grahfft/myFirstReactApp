ebdf8539d08f3d2d988e0a81b9ebad243756e93d
Dennis Lassell <dennis.lassell@gmail.com>
2018-08-27 20:34:51 -0400

    my first commit

---
+++ src/MyNewComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./MyNewComponent.css"
+
+export default class MyNewComponent extends Component
+{
+    render(){
+        return (
+            <div className="MyNewComponent">
+                <h1 className="myfirstTitle"> Hello World </h1>
+            </div>
+        )
+    }
+}
